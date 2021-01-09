9c96c292c7a2b59de193021560af3b582cb0b1e7
JinGu <xortm854@naver.com>
2021-01-09 19:48:00 +0900

    [Algorithm] PQ 이중우선순위큐 구현
    PQ 의 직접적인 구조를 구현한 것은 아니지만 탐색을 통해 최솟값 최댓값을 가져오도록 구현하였습니다.

---
+++ Algorithm/PriorityQueue/이중우선순위큐.js
@@ -0,0 +1,66 @@
+class PriorityQueue{
+    constructor(){
+        this.arr = [];
+    }
+    enqueue(value){
+        this.arr.push(value);
+    }
+    dequeueMax(){
+        if(this.arr.length === 0 ) return;
+        let save = -1, Max = -2000000000;
+        this.arr.forEach((elem,idx)=>{
+            if(elem > Max){
+                Max = elem;
+                save = idx;
+            }
+        });
+        return this.arr.splice(save,1);
+    }
+
+    dequeueMin(){
+        if(this.arr.length === 0 ) return;
+        let save = -1, Min = 2000000000;
+        this.arr.forEach((elem,idx)=>{
+            if(elem < Min){
+                Min = elem;
+                save = idx;
+            }
+        });
+        return this.arr.splice(save,1);
+    }
+
+    returnAnswer(){
+        if(this.arr.length == 0 ) return [0,0];
+        const Max = this.dequeueMax();
+        const Min = this.dequeueMin();
+        return [...Max, ...Min];
+    }
+}
+
+const pq = new PriorityQueue();
+
+function parsing(str){
+    const list = str.split(' ');
+    return { cmd: list[0], val: parseInt(list[1])};
+}
+
+function execution(cmd, val){
+    switch(cmd){
+        case 'I':
+            pq.enqueue(val);
+            break;
+        case 'D':
+            if(val == 1) pq.dequeueMax();
+            else pq.dequeueMin();
+            break;
+    }
+}
+
+function solution(operations) {
+    var answer = [];
+    operations.forEach((oper)=>{
+        const { cmd, val } = parsing(oper);
+        execution(cmd,val);
+    });
+    return pq.returnAnswer();
+}
